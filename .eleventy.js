6c324d13079bb77958ba5b6bfba5f3b317f4f44a
Alexandr Gakhov <alexandrgakhov@Alexandrs-MacBook-Pro.local>
2022-10-12 23:26:16 +0600

    index page

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/font");
+
+  eleventyConfig.addPassthroughCopy("./src/admin");
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
